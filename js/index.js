a9f9fe615922860849c3d375fe7626d194307310
Alwong <alwong@ya.ru>
2022-11-27 16:52:17 +0400

    cleaned code

--- js/index.js
+++ js/index.js
@@ -1,33 +1,9 @@
-const popup = document.querySelector('.popup');
 const menuBtn = document.querySelector('.nav-panel__menu-btn');
 const menuList = document.querySelector('.nav-panel__list');
 const homeLink = document.querySelector('.nav-panel__home');
-const participants = document.querySelector('.participants');
 const navPanelContainer = document.querySelector('.nav-panel__container');
-let modalBtnOk;
 
 menuBtn.addEventListener('click', toggleMenu);
-participants.addEventListener('click', openModal);
-
-function openModal() {
-    participants.removeEventListener('click', openModal);
-    popup.innerHTML = 
-    `
-    <div class="background">
-        <div class="modal">
-            <p>Пока нет участников</p>
-            <button class="modal-btn-ok">Ок</button>
-        </div>
-    </div>
-    `;
-    modalBtnOk = document.querySelector('.modal-btn-ok');
-    modalBtnOk.addEventListener('click', closeModal);
-}
-function closeModal() {
-    modalBtnOk.removeEventListener('click', closeModal);
-    popup.innerHTML = ``;
-    participants.addEventListener('click', openModal);
-}
 
 function toggleMenu() {
     if (menuBtn.classList.contains('active')) {
